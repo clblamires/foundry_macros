1710daf83fdefaa6cdddafe3e7071ac1c9ac818b
Casey Blamires <clblamires@lcmail.lcsc.edu>
2024-02-23 18:30:46 -0800

    Create macro.js

---
+++ macro.js
@@ -0,0 +1,11 @@
+const messages = [
+  "I AM THE ALPHA NOW!",
+  "Don't worry! Crying is a free action.",
+  "You talk a lot of crap for someone in fireball range",
+  "You see that guy? (points at Actaeon) He's gonna stab you. A lot.",
+  "Alright, it's PEW PEW PEW time!"
+];
+const m = Math.floor( Math.random() * messages.length );
+ChatMessage.create({
+ content: "MACRO TEST / " + messages[m]
+});
