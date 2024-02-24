66534ea20e4097fdf15dd0f4c6590a13d23f039c
Casey Blamires <clblamires@lcmail.lcsc.edu>
2024-02-23 18:35:34 -0800

    Update macro.js

--- macro.js
+++ macro.js
@@ -3,7 +3,8 @@ const messages = [
   "Don't worry! Crying is a free action.",
   "You talk a lot of crap for someone in fireball range",
   "You see that guy? (points at Actaeon) He's gonna stab you. A lot.",
-  "Alright, it's PEW PEW PEW time!"
+  "Alright, it's PEW PEW PEW time!",
+  "Fluffy and Piddles! I CHOOSE YOU!"
 ];
 const m = Math.floor( Math.random() * messages.length );
 ChatMessage.create({
