27653c23151fd6fe5649c79726e7080684b32d90
Casey Blamires <clblamires@lcmail.lcsc.edu>
2024-03-01 11:20:39 -0800

    Create battlecry.js

---
+++ battlecry.js
@@ -0,0 +1,5 @@
+ChatMessage.create({
+  content: '<h2>LEROY JENKINS!!!!!!</h2>'
+});
+// Replace h2 with h1...h6, depending on how big you want the message to be.
+// And replace LEROY JENKINS with whatever other battlecry you prefer! ^_^
