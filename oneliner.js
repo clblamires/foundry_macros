89562b702c65e3ede3923cf57aca7e781c444d5f
Casey Blamires <clblamires@lcmail.lcsc.edu>
2024-03-02 16:15:28 -0800

    Update oneliner.js

--- oneliner.js
+++ oneliner.js
@@ -2,13 +2,15 @@
 // use " " quotes to wrap the line, and add a comma at the end
 const messages = [
   "I AM THE ALPHA NOW!",
-  "Don't worry! Crying is a free action.",
-  "You talk a lot of crap for someone in fireball range",
+  "Oh, you do that, darling...",
+  "Fireball time!",
   "You see that guy? (points at Actaeon) He's gonna stab you. A lot.",
   "Alright, it's PEW PEW PEW time!",
-  "Fluffy and Piddles! I CHOOSE YOU!"
+  "Yum yum!!!"
 ];
 const m = Math.floor( Math.random() * messages.length );
 ChatMessage.create({
- content: messages[m]
+ content: messages[m],
+ emote: true,
+ flavor: "<h4>Leef's One-Liner!</h4>"
 });
